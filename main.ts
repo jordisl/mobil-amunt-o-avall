5fd06a9edaeb91c38792dc8a9e82e7f8275c98b1
jordisl <72779969+jordisl@users.noreply.github.com>
2020-10-13 06:49:36 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+basic.forever(function () {
+	
+})
